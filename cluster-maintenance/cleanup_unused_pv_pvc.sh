31e0bed48ded5715199d5bad04c2c7d8568d0b97
Flávio Ferreira <flaviommferreira@gmail.com>
2020-04-02 15:38:49 +0100

    Improve script

--- cluster-maintenance/cleanup_unused_pv_pvc.sh
+++ cluster-maintenance/cleanup_unused_pv_pvc.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 function output_help {
     echo "Usage: `basename $0` <kubernetes_namespace_to_clean>";
 }
@@ -11,33 +15,50 @@ fi
 
 NAMESPACE=$1
 
-CLAIMS_IN_USE=$(kubectl get pods -n $NAMESPACE -o=json | jq -c '.items[] | {claimName: .spec.volumes[] | select( has ("persistentVolumeClaim") ).persistentVolumeClaim.claimName }.claimName')
-ALL_CLAIMS=$(kubectl get pvc -n $NAMESPACE | tail -n +2 | awk '{print $1}')
+claims_in_use=$(kubectl get pods -n $NAMESPACE -o=json | jq -c '.items[] | {claimName: .spec.volumes[] | select( has ("persistentVolumeClaim") ).persistentVolumeClaim.claimName }.claimName')
+all_claims=$(kubectl get pvc -n $NAMESPACE | tail -n +2 | awk '{print $1}')
 
-echo "--------- CLAIMS IN USE ---------"
-echo $CLAIMS_IN_USE
-
-echo "> Going to delete claims"
-for claim_to_delete in $ALL_CLAIMS
+claims_to_delete=()
+for claim in $all_claims
 do
-	to_delete=true
-    for claim_in_use in $CLAIMS_IN_USE
-	do
-		if [[ "\""$claim_to_delete"\"" == $claim_in_use ]]
-		then
-			echo "Skipping claim "$claim_to_delete
-	    	to_delete=false
-    	fi 
-	done
+    to_delete=true
+    for claim_in_use in $claims_in_use
+    do
+    	if [[ "\""$claim"\"" == $claim_in_use ]]
+        then
+            to_delete=false
+        fi 
+    done
 
-	if $to_delete
-	then
-    	echo "Going to delete "$claim_to_delete
-    	kubectl -n $NAMESPACE delete pvc $claim_to_delete
-	fi 
+    if $to_delete
+    then
+    	claims_to_delete+=($claim)
+    fi 
 done
 
-echo "> Going to clean up unbound PVs"
-kubectl -n $NAMESPACE get pv | tail -n +2 | grep -v Bound | \
-  awk '{print $1}' | xargs -I{} kubectl -n $NAMESPACE delete pv {}
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
+
+if [ ${#claims_to_delete[@]} -eq 0 ]
+then
+	echo "No PersistentVolumeClaim to delete"
+else
+	printf "${RED}Going to delete the following claims:\n"
+    for c in "${claims_to_delete[@]}"; do printf "${RED}$c${NC}\n"; done
+    echo -n "Are you sure you want to clean this claims... [ENTER]"
+    read ready
+	kubectl -n $NAMESPACE delete pvc ${claims_to_delete[*]}	
+fi
+
+pv_to_delete=($(kubectl -n $NAMESPACE get pv | tail -n +2 | grep -v Bound | awk '{print $1}'))
+if [ ${#pv_to_delete[@]} -eq 0 ]
+then
+	echo "No PersistentVolume to delete"
+else
+    printf "${RED}Going to clean up unbound PVs"
+    for c in "${pv_to_delete[@]}"; do printf "${RED}$c${NC}\n"; done
+    echo -n "Are you sure you want to clean this pvs... [ENTER]"
+    read ready
+    kubectl -n $NAMESPACE delete pv ${pv_to_delete[*]}
+fi
